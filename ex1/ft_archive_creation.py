a267b16cac2c0477058ffff49aa6c9d41d5df090
K4elts <andreslt310@gmail.com>
2026-09-16 13:25:23 +0200

    add ex1, modify ex0

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -1,6 +1,14 @@
 import sys
 
 
+def save_data() -> None:
+    print("---------------------")
+    file = open(sys.argv[1], "r+")
+    content = file.readline()
+    print(f"{content}")
+    file.close()
+
+
 def read_text() -> None:
     try:
         print(f"Accessing file: '{sys.argv[1]}'")
@@ -11,6 +19,7 @@ def read_text() -> None:
         print("\n---")
         file.close()
         print(f"File {sys.argv[1]} closed")
+        save_data()
     except IndexError:
         print(f"Usage: {sys.argv[0]} <file>")
     except FileNotFoundError as e:
@@ -20,7 +29,7 @@ def read_text() -> None:
 
 
 if __name__ == "__main__":
-    print("=== Cyber Archives Recovery ===")
+    print("=== Cyber Archives Recovery & Preservation ===")
     if len(sys.argv) == 2:
         read_text()
     else:
